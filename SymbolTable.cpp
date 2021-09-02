ea55395a3431a1a92b734f22c1bcbd74a128f740
tuong.lau16072002@hcmut.edu.vn <tuong.lau16072002@hcmut.edu.vn>
2021-09-02 09:01:36 +0700

    first-commit

---
+++ SymbolTable.cpp
@@ -0,0 +1,40 @@
+#include "SymbolTable.h"
+
+Node *SymbolTable::insert(Node *head, Node *T) {
+  Node *p = head;
+
+  if (head == NULL) {
+    head = T;
+    head-> next = NULL;
+    return head;
+  }
+  p->next = T;
+
+  return head;
+}
+void SymbolTable::print(Node *head) {
+  Node *p = head;
+  while (p != NULL) {
+    cout << p->method << " " << p->indentifier_name << " " << p->type << endl;
+    p = p->next;
+  }
+}
+
+void SymbolTable::run(string filename) {
+
+  fstream newfile;
+  newfile.open(filename, ios::in);
+  if (newfile.is_open()) {
+    string dataLine, method, indentifier_name, type;
+    while (getline(newfile, dataLine)) {
+    istringstream iss(dataLine);
+
+        while (data)
+    //   Node *T = new Node(method, indentifier_name, type);
+    //   head = insert(head, T);
+    }
+    // print(head);
+    newfile.close();
+  }
+  cout << "success";
+};
---
+++ SymbolTable.h
@@ -0,0 +1,36 @@
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+#include "main.h"
+const int MAX = 100;
+
+class Node {
+private:
+  string method, indentifier_name, type;
+  Node *next;
+
+public:
+  Node() { next = NULL; }
+  Node(string method, string indentifier_name, string type) {
+    this->method = method;
+    this->indentifier_name = indentifier_name;
+    this->type = type;
+    next = NULL;
+  }
+  friend class SymbolTable;
+};
+
+class SymbolTable {
+private:
+  Node *head, *tail;
+  Node *next;
+
+public:
+  SymbolTable() {
+    head = NULL;
+    tail = NULL;
+  }
+  void run(string filename);
+  Node* insert(Node* head, Node* T);
+  void print(Node* head);
+};
+#endif
